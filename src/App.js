c7a7d716f34b3ba31f0d3a5501e6cfeb1109abf0
olivercastroteran <olivercastroteran@gmail.com>
2020-09-01 09:50:54 -0400

    fetching notes from firebase db on snapShot and save it to state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const firebase = require('firebase');
 class App extends Component {
-  state = {};
+  state = {
+    selectedNoteIndex: null,
+    slectedNote: null,
+    notes: null,
+  };
+
   render() {
     return <div>Hollo</div>;
   }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection('notes')
+      .onSnapshot((serverUpdate) => {
+        const notes = serverUpdate.docs.map((doc) => {
+          const data = doc.data();
+          data['id'] = doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({ notes: notes });
+      });
+  };
 }
 
 export default App;
